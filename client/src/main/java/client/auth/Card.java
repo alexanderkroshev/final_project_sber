21127f46bee24701bf8e2d215d1385d661724916
alexanderkroshev <alexanderkroshev@gmail.com>
2022-03-29 23:53:36 +0700

    рабочий клиент без кэша токена

--- client/src/main/java/client/auth/Card.java
+++ client/src/main/java/client/auth/Card.java
@@ -3,12 +3,9 @@ package client.auth;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
 @Getter
 @AllArgsConstructor
 public class Card {
-	private String cardNumber;
-	private String cardPassword;
-
-
+    private String cardNumber;
+    private String cardPassword;
 }
--- client/src/main/java/client/controller/ATMController.java
+++ client/src/main/java/client/controller/ATMController.java
@@ -1,21 +1,12 @@
 package client.controller;
 
-import client.auth.Card;
-import client.auth.ResponseToken;
 import client.dto.BalanceDTO;
 import client.service.CardServiceApi;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Objects;
-
 
 @RestController
 @AllArgsConstructor
@@ -25,7 +16,7 @@ public class ATMController {
     private final CardServiceApi cardService;
 
     @GetMapping("/balance/{number}/{password}")
-    public BalanceDTO getBalance(@PathVariable String number,@PathVariable String password) {
+    public BalanceDTO getBalance(@PathVariable String number, @PathVariable String password) {
         return cardService.getBalance(number, password);
     }
 }
