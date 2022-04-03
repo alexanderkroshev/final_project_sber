1cc8384b5d8f3dc1d5987d1a3dadb2bf92b4b97e
alexanderkroshev <alexanderkroshev@gmail.com>
2022-04-03 12:27:28 +0700

    небольшие изменения

---
+++ client/src/main/java/client/exception/AuthorizationException.java
@@ -0,0 +1,13 @@
+package client.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class AuthorizationException extends RuntimeException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
+
