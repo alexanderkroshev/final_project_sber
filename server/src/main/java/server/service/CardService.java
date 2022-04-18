c4b3b7413e2c2d9303448aaca3d623901892f5dd
alexanderkroshev <alexanderkroshev@gmail.com>
2022-04-18 18:40:15 +0700

    добавил исключения при добавлении карт и пользователей

--- server/src/main/java/server/service/CardService.java
+++ server/src/main/java/server/service/CardService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import server.dto.UserCardDto;
 import server.exception.CardNotFoundException;
+import server.exception.CardSaveException;
 import server.model.Card;
 import server.repository.CardRepository;
 
@@ -20,7 +21,15 @@ public class CardService {
     }
 
     public void saveCard(String cardNumber, String cardPassword, Long userId) {
-        cardRepository.saveCard(cardNumber, cardPassword, userId);
+        if (!cardNumber.matches("[0-9]+"))
+            throw new CardSaveException("card number should contain only numbers");
+        if (cardPassword == null || userId == null)
+            throw new CardSaveException("card should not have null values");
+        try {
+            cardRepository.saveCard(cardNumber, cardPassword, userId);
+        } catch (Exception e) {
+            throw new CardSaveException("problem with save card");
+        }
     }
 
     public List<UserCardDto> findUserCards(Long userId) {
--- server/src/main/java/server/service/UserService.java
+++ server/src/main/java/server/service/UserService.java
@@ -2,7 +2,9 @@ package server.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import server.exception.CardSaveException;
 import server.exception.UserNotFoundException;
+import server.exception.UserSaveException;
 import server.model.User;
 import server.repository.UserRepository;
 
@@ -17,6 +19,14 @@ public class UserService {
     }
 
     public void saveUser(String login, String password, String name, String surname) {
-        userRepository.saveUser(login, password, name, surname);
+        if (login.matches("[0-9]+"))
+            throw new UserSaveException("user login should not contain only numbers");
+        if (login == null || password == null || name == null || surname == null)
+            throw new UserSaveException("user should not have null values");
+        try {
+            userRepository.saveUser(login, password, name, surname);
+        } catch (Exception e) {
+            throw new UserSaveException("problem with save user");
+        }
     }
 }
